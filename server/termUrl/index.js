ca4a629939f27b89039452396503f74c963849e3
Other <other@Others-MacBook-Pro.local>
2019-12-08 18:59:15 -0500

    term url integration complete

--- server/termUrl/index.js
+++ server/termUrl/index.js
@@ -5,57 +5,38 @@ const termUrlRouter = express.Router();
 
 termUrlRouter
   .route('')
-  .post(async (req, res) => {
+  .get(async (req, res) => {
     try {
-    // reference from own env. update given prod v sandbox env
-    let secretKey = process.env.SANDBOX_SECRET_KEY; 
-    let headers = {
-      headers: {
-        Authorization: `Bearer ${secretKey}`
-      }
-    }
-   
-    // reference from card form "tokenizationSuccess" event data in App.js
-    console.log(req.body);
-    let tokenObject = req.body.tokenObject;
-
-    // current options: usd, eur, gbp
-    let fiatBaseCurrency = 'usd';
-
-    // convention is to scale charged amount by 100 
-    // to remove decimal considerations
-    // so this represents $10 for example
-    let fiatChargeAmount = '1000';
     
-    // reference from own contact. update given prod v sandbox env
-    let contactId = process.env.SANDBOX_CONTACT_ID; 
     let data = {
-      tokenObject, fiatBaseCurrency, contactId, fiatChargeAmount
+      orderId: req.query.orderId
     };
+    console.log(data);
 
     // update given prod v sandbox env (https://api.carbon.money v https://sandbox.carbon.money)
-    let url = 'https://sandbox.carbon.money/v1/card/checkout'
+    let url = 'https://sandbox.carbon.money/v1/card/charge3d/complete'
 
+    // if you want to cancel order call the cancel order endpoint
     let resp;
     try {
-      resp = await axios.post(url, data, headers);
+      resp = await axios.post(url, data);
       let respData = resp.data;
-      console.log('Checkout endpoint success resp data')
+      console.log('Term url endpoint success resp data')
       console.log(respData);
       return res.send(respData);
     } catch(err) {
       let errResponse = err.response;
       if (errResponse) {
-        console.log('Error resp data for checkout endpoint', errResponse.data);
-        console.log('Error resp status for checkout endpoint', errResponse.status);
+        console.log('Error resp data for charge 3d complete endpoint', errResponse.data);
+        console.log('Error resp status for charge 3d complete endpoint', errResponse.status);
       } else {
-        console.log('Error calling checkout endpoint', err);
+        console.log('Error calling charge 3d complete endpoint at term url', err);
       }      
       return res.status(errResponse.status).send(errResponse.data);
     }
     
   } catch(err) {
-    console.log(`Error charging card`);
+    console.log(`Error at term url.`);
     console.log(err);
   }
 });
